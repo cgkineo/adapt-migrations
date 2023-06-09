5cb72fbdb0847fc26ba2cc03527da5add6b02ca7
Oliver Foster <oliver.foster@kineo.com>
2023-06-09 15:19:52 +0100

    Fixed stack traces on errors

--- api/errors.js
+++ api/errors.js
@@ -1,9 +1,15 @@
 import { deferOrRunWrap, successStopOrErrorWrap } from '../lib/lifecycle.js'
 
 export function throwError(description) {
+  let error = description;
+  if (!(description instanceof Error)) {
+    error = new Error(description);
+  } else {
+    description = description.message
+  }
   return deferOrRunWrap(function(context) {
     return successStopOrErrorWrap('throwError', description, async () => {
-      throw new Error(description);
+      throw error;
     });
   }, { type: 'action' });
 }
