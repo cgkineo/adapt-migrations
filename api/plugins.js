8d94c914993eab22f6e7395a2dd70a3633175afd
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2024-03-01 15:14:38 +0000

    remove extra space

--- api/plugins.js
+++ api/plugins.js
@@ -19,7 +19,7 @@ export function addPlugin (description, config) {
 
       if (!description || !config) throw new Error('addPlugin - incorrectly configured')
 
-      const newPlugin = context.toPlugins.find(plugin => (plugin.name === config.name) )
+      const newPlugin = context.toPlugins.find(plugin => (plugin.name === config.name))
       if (!newPlugin) throw new Error(`addPlugin - ${config.name} not found`)
       context.fromPlugins.push(newPlugin[0])
     })
