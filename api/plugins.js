200af2c8dae3543042d2ba94ecd3cd264f3567c8
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2024-03-01 14:59:36 +0000

    remove check for displayName on addPlugin

--- api/plugins.js
+++ api/plugins.js
@@ -19,7 +19,7 @@ export function addPlugin (description, config) {
 
       if (!description || !config) throw new Error('addPlugin - incorrectly configured')
 
-      const newPlugin = context.toPlugins.find(plugin => (plugin.name === config.name || plugin.displayName?.toUpperCase() === config.name?.toUpperCase() ))
+      const newPlugin = context.toPlugins.find(plugin => (plugin.name === config.name))
       if (!newPlugin) throw new Error(`addPlugin - ${config.name} not found`)
       context.fromPlugins.push(newPlugin[0])
     })
