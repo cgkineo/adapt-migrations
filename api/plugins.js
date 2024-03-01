9806c8289aced1a6b811daadf84ddf32cdc31f39
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2024-03-01 11:44:20 +0000

    Fixes applied

--- api/plugins.js
+++ api/plugins.js
@@ -4,13 +4,9 @@ export function removePlugin (description, config) {
   return deferOrRunWrap(function (context) {
     return successStopOrErrorWrap('removePlugin', description, async () => {
 
-      if(!description || !config) throw new Error('removePlugin - incorrectly configured')
+      if (!description || !config) throw new Error('removePlugin - incorrectly configured')
 
-      context.fromPlugins = context.fromPlugins.filter((plugin) => {
-        if(plugin.name !== config.name) return plugin
-      });
-
-      context.setFromPlugins();
+      context.fromPlugins = context.fromPlugins.filter(plugin => plugin.name !== config.name)
 
       return true
     })
@@ -21,15 +17,11 @@ export function addPlugin (description, config) {
   return deferOrRunWrap(function (context) {
     return successStopOrErrorWrap('addPlugin', description, async () => {
 
-      if(!description || !config) throw new Error('addPlugin - incorrectly configured')
+      if (!description || !config) throw new Error('addPlugin - incorrectly configured')
 
-      const newPlugin = context.toPlugins.filter((plugin) => (plugin.name === config.name || plugin.displayName?.toUpperCase() === config.name?.toUpperCase() ));
-      if(newPlugin.length === 0) throw new Error(`addPlugin - ${config.name} not found`)
+      const newPlugin = context.toPlugins.filter(plugin => (plugin.name === config.name || plugin.displayName?.toUpperCase() === config.name?.toUpperCase() ));
+      if (newPlugin.length === 0) throw new Error(`addPlugin - ${config.name} not found`)
       context.fromPlugins.push(newPlugin[0]);
-
-      context.setFromPlugins();
-
-      return true
     })
   }, { description, type: 'action' })
 };
@@ -38,20 +30,14 @@ export function updatePlugin (description, config) {
   return deferOrRunWrap(function (context) {
     return successStopOrErrorWrap('updatePlugin', description, async () => {
 
-      if(!description || !config) throw new Error('Update Plugin call incorrectly configured')
+      if (!description || !config) throw new Error('updatePlugin - incorrectly configured')
 
       context.fromPlugins.forEach((plugin) => {
-        if(plugin.name !== config.name) return plugin
-
+        if (plugin.name !== config.name) return
         plugin.version = config.version
-
-        if (config.framework) {
-          plugin.framework = config.framework
-        }
-        return plugin
+        if (!config.framework) return
+        plugin.framework = config.framework
       });
-
-      context.setFromPlugins();
 
       return true
     })
