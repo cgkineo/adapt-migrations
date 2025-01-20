405fff74c44d0a802156aa039ae603b58e5bec9e
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-01-20 16:13:51 +0000

    Update function to combine content.find()
    Co-authored-by: Oliver Foster <oliver.foster@kineo.com>

--- api/helpers.js
+++ api/helpers.js
@@ -1,7 +1,3 @@
 export function getConfig (content) {
-  const configType = content.find(({ _type }) => _type === 'config')
-  if (configType) return configType
-
-  const config = content.find(({ __path__ }) => __path__.endsWith('config.json'))
-  return config
+  return content.find(({ _type, __path__ }) => _type === 'config' || __path__.endsWith('config.json'))
 }
