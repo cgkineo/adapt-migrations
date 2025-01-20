2a91c3e171489550f6c8aebdcb8431411e3921a8
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-01-20 11:46:17 +0000

    helper typo

--- api/helpers.js
+++ api/helpers.js
@@ -1,6 +1,6 @@
 export function getConfig (content) {
-  const configType = content.find(({ _type }) => _type === 'config');
-  const config = (!configType) ? configType : content.find(({ __path__ }) => __path__ === 'src/course/config.json')
+  const configType = content.find(({ _type }) => _type === 'config')
+  const config = (!configType) ? content.find(({ __path__ }) => __path__ === 'src/course/config.json') : configType
 
   return config
 }
