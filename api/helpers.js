4309a6bf6a123cdbd7742d314f281e5849132a1e
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-01-20 12:02:58 +0000

    Switch from ternary to return early

--- api/helpers.js
+++ api/helpers.js
@@ -1,6 +1,7 @@
 export function getConfig (content) {
   const configType = content.find(({ _type }) => _type === 'config')
-  const config = (!configType) ? content.find(({ __path__ }) => __path__ === 'src/course/config.json') : configType
+  if (configType) return configType
 
+  const config = content.find(({ __path__ }) => __path__ === 'src/course/config.json')
   return config
 }
