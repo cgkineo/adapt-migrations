7201f9f1b9dc8a791e5cd37fc9294042658adde4
Brad Simpson <brad.simpson@kineo.com>
2025-01-29 09:57:55 -0700

    Replace getGlobals with getCourse
    Co-authored-by: Oliver Foster <oliver.foster@kineo.com>

--- api/helpers.js
+++ api/helpers.js
@@ -2,7 +2,6 @@ export function getConfig (content) {
   return content.find(({ _type, __path__ }) => _type === 'config' || __path__.endsWith('config.json'))
 }
 
-export function getGlobals (content) {
-  const course = content.find(({ _type, __path__ }) => _type === 'course' || __path__.endsWith('course.json'));
-  return course?._globals;
+export function getCourse(content) {
+  return content.find(({ _type }) => _type === 'course');
 }
